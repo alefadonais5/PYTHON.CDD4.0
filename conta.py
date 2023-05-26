30060670698422f3d0c53f0589cfddecd41ec5c5
Alef Adonais <alefadonais5@gmail.com>
2023-05-26 11:52:31 -0300

    CDD 4.0 - POO
    CDD 4.0 - POO

--- conta.py
+++ conta.py
@@ -1,19 +1,39 @@
 class Conta:
-    def __init__(self, Numero_Conta, Saldo, Status, Nome_Cliente, Tipo_Conta):
+    def __init__(self, Numero_Conta, Saldo, Status, Nome_Cliente, Tipo_Conta, Limite=0, Limite_Usado=0):
 
         self.Nome_Cliente = Nome_Cliente
         self.Numero_Conta = Numero_Conta
         self.Saldo = Saldo
         self.Status = Status
         self.Tipo_Conta = Tipo_Conta
+        self.Limite = Limite
+        self.Limite_Usado = Limite_Usado
 
     def Depositar(self, Deposito):
-        self.Saldo = Deposito+self.Saldo
-        return self.Saldo
+        if self.Status == "Ativado":
+            if self.Saldo >= 0:
+                self.Saldo = Deposito+self.Saldo
+                return self.Saldo
+            else:
+                self.Limite_Usado = Deposito+self.Saldo #corrigir essa parte
+                self.Saldo = self.Saldo + Deposito
+                return self.Limite_Usado, self.Saldo
+
+        else:
+            print("Conta está desativada")
 
     def Sacar(self, Saque):
-        self.Saldo = self.Saldo-Saque
-        return self.Saldo
+        if self.Status == "Ativado":
+            if Saque < self.Saldo or Saque == self.Saldo:
+                self.Saldo = self.Saldo-Saque
+                return self.Saldo
+            else:
+                self.Limite_Usado = Saque-self.Saldo
+                self.Saldo = self.Saldo-Saque
+                return self.Limite_Usado, self.Saldo
+
+        else:
+            print("Conta está desativada")
 
     def Desativar (self):
         if self.Status == "Ativado":
@@ -30,6 +50,32 @@ class Conta:
             self.Status = "Ativado"
         else:
             print("A conta já está ativada.")
+    def Verificar(self):
+        if self.Status == "Ativado":
+            if self.Saldo > 0:
+                print(f"Seu saldo é R${self.Saldo}.")
+            else:
+                print(f"Seu saldo está negativo no valor de R${self.Saldo}")
+        else:
+            print("Conta desativada, não tem saldo....")
 
-C1 = Conta(254, 1500, "Ativado", "Cliente 1", "Corrente")
-C2 = Conta(253,1000,"Ativado", "Opal", "Poupança")
+    def Ativar_Limite(self, Limite):
+        if self.Saldo != 0 and self.Status == "Ativado":
+            self.Limite = Limite
+        else:
+            print("Sua conta está desativada, não pode ser utilizada.")
+
+    def Desativar_Limite(self):
+        if self.Limite > 0:
+            self.Limite = False
+            return self.Limite
+
+
+C1 = Conta(254,500, "Ativado", "Cliente 1", "Corrente")
+# C2 = Conta(253,1000,"Ativado", "Opal", "Poupança")
+C1.Ativar_Limite(300)
+var = C1.Sacar(700)
+print(var)
+
+var1 = C1.Depositar(100)
+print(var1)
